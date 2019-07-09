d08a14d0230cd6d5e5681ff952a34d402e459956
Bohdan Moskalenko <bohdanmoskalenko@Bohdans-MacBook-Pro.local>
2019-07-09 18:43:26 +0300

    Set Headers

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'twig');
 
